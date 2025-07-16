3d27bdae9d4394f599a03f4bf6f9fc931e4f0c69
Anky2231 <akriti31akutigga@gmail.com>
2025-07-16 23:18:04 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -1,21 +1,27 @@
-function dis(n)
-{
-    console.log(`Button ${n} pressed`)
-    document.getElementById("result").value +=n
+
+function dis(n) {
+    console.log(`Button ${n} pressed`);
+    document.getElementById("expression").value += n;
 }
 
-function solve{
+
+function clr() {
+    document.getElementById("expression").value = "";
+    document.getElementById("result").value = "";
+}
+
+
+function solve() {
     try {
-        let expression = document.getElementById("result").value;
-        let result = eval(expression); 
-        document.getElementById("result").value = result;
-        console.log(`Expression: ${expression}, Result: ${result}`);
+        const expr = document.getElementById("expression").value;
+        const output = eval(expr); 
+
+        
+        document.getElementById("result").value = `= ${output}`;
+        console.log(`${expr} = ${output}`);
     } catch (err) {
         document.getElementById("result").value = "Error";
     }
 }
-    
-function clr() {
-    document.getElementById("result").value = "";
-}
+
 
