be7cf4f2dd5bae78b833cb5d42ef4abcd2f0bdff
Anky2231 <akriti31akutigga@gmail.com>
2025-07-16 23:11:03 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -3,4 +3,19 @@ function dis(n)
     console.log(`Button ${n} pressed`)
     document.getElementById("result").value +=n
 }
-function solve
+
+function solve{
+    try {
+        let expression = document.getElementById("result").value;
+        let result = eval(expression); 
+        document.getElementById("result").value = result;
+        console.log(`Expression: ${expression}, Result: ${result}`);
+    } catch (err) {
+        document.getElementById("result").value = "Error";
+    }
+}
+    
+function clr() {
+    document.getElementById("result").value = "";
+}
+
